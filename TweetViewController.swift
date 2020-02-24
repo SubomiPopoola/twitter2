386e658c57c206c62da38671a8e163a07e47d8d3
SubomiPopoola <60233501+SubomiPopoola@users.noreply.github.com>
2020-02-24 01:00:07 -0500

    Add files via upload

---
+++ TweetViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TweetViewController.swift
+//  Twitter
+//
+//  Created by Subomi Popoola on 2/23/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController {
+
+    @IBOutlet weak var tweetTextView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tweetTextView.becomeFirstResponder()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func tweet(_ sender: Any) {
+        if (!tweetTextView.text.isEmpty){
+                   TwitterAPICaller.client?.postTweet(tweetString: tweetTextView.text, success: {
+                       self.dismiss(animated: true, completion: nil)
+                   }, failure: { (error) in
+                       print("Error posting Tweet \(error)")
+                       self.dismiss(animated: true, completion: nil)
+                   })
+               }
+        else{
+                   self.dismiss(animated: true, completion: nil)
+               }
+    }
+}
